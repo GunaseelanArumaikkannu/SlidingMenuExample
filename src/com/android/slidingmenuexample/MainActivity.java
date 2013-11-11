ee3574c21b035163d489ccc1f93e85442799599a
Gunaseelan A <gunasiet@gmail.com>
2013-11-11 09:02:35 +0530

    Commit 3

--- src/com/android/slidingmenuexample/MainActivity.java
+++ src/com/android/slidingmenuexample/MainActivity.java
@@ -1,6 +1,5 @@
 package com.android.slidingmenuexample;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -16,124 +15,96 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
-// DK 
-// This is my studying about Sliding Menu following Youtube video
 public class MainActivity extends FragmentActivity {
 
-    // The MainLayout which will hold both the sliding menu and our main content
-    // Main content will holds our Fragment respectively
-    MainLayout mLayout;
-    
-    // ListView menu
-    private ListView lvMenu;
-    private String[] lvMenuItems;
-    
-    // Menu button
-    Button btMenu;
-    
-    // Title according to fragment
-    TextView tvTitle;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Inflate the mainLayout
-        mLayout = (MainLayout)this.getLayoutInflater().inflate(R.layout.activity_main, null);
-        setContentView(mLayout);
-        
-        // Init menu
-        
-        lvMenuItems = getResources().getStringArray(R.array.menu_items);
-        lvMenu = (ListView) findViewById(R.id.menu_listview);
-        lvMenu.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, lvMenuItems));
-        lvMenu.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                onMenuItemClick(parent, view, position, id);
-            }
-            
-        });
-        
-        
-        // Get menu button
-        btMenu = (Button) findViewById(R.id.button_menu);
-        btMenu.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Show/hide the menu
-                toggleMenu(v);
-            }
-        });
-        
-        // Get title textview
-        tvTitle = (TextView) findViewById(R.id.activity_main_content_title);
-        
-        
-        // Add FragmentMain as the initial fragment       
-        FragmentManager fm = MainActivity.this.getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        
-        Layout1 fragment = new Layout1();
-        ft.add(R.id.activity_main_content_fragment, fragment);
-        ft.commit();   
-        
-    }
+	MainLayout mLayout;
+	private ListView lvMenu;
+	private String[] lvMenuItems;
+	Button btMenu;
+	TextView tvTitle;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mLayout = (MainLayout) this.getLayoutInflater().inflate(
+				R.layout.activity_main, null);
+		setContentView(mLayout);
 
-    public void toggleMenu(View v){
-        mLayout.toggleMenu();
-    }
-    
-    // Perform action when a menu item is clicked
-    private void onMenuItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String selectedItem = lvMenuItems[position];
-        String currentItem = tvTitle.getText().toString();
-        
-        // Do nothing if selectedItem is currentItem
-        if(selectedItem.compareTo(currentItem) == 0) {
-            mLayout.toggleMenu();
-            return;
-        }
-            
-        FragmentManager fm = MainActivity.this.getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        Fragment fragment = null;
+		lvMenuItems = getResources().getStringArray(R.array.menu_items);
+		lvMenu = (ListView) findViewById(R.id.menu_listview);
+		lvMenu.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, lvMenuItems));
+		lvMenu.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				onMenuItemClick(parent, view, position, id);
+			}
 
-        
-        if(selectedItem.compareTo("Layout 1") == 0) {
-            fragment = new Layout1();
-        } else if(selectedItem.compareTo("Layout 2") == 0) {
-            fragment = new Layout2();
-        }
-        
-        if(fragment != null) {
-            // Replace current fragment by this new one
-            ft.replace(R.id.activity_main_content_fragment, fragment);
-            ft.commit();
-            
-            // Set title accordingly
-            tvTitle.setText(selectedItem);
-        }
-        
-        // Hide menu anyway
-        mLayout.toggleMenu();
-    }
-    
-    @Override
-    public void onBackPressed() {
-        if (mLayout.isMenuShown()) {
-            mLayout.toggleMenu();
-        }
-        else {
-            super.onBackPressed();
-        }
-    }
+		});
+
+		btMenu = (Button) findViewById(R.id.button_menu);
+		btMenu.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Show/hide the menu
+				toggleMenu(v);
+			}
+		});
+
+		tvTitle = (TextView) findViewById(R.id.activity_main_content_title);
+
+		FragmentManager fm = MainActivity.this.getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		Layout1 fragment = new Layout1();
+		ft.add(R.id.activity_main_content_fragment, fragment);
+		ft.commit();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void toggleMenu(View v) {
+		mLayout.toggleMenu();
+	}
+
+	private void onMenuItemClick(AdapterView<?> parent, View view,
+			int position, long id) {
+		String selectedItem = lvMenuItems[position];
+		String currentItem = tvTitle.getText().toString();
+		if (selectedItem.compareTo(currentItem) == 0) {
+			mLayout.toggleMenu();
+			return;
+		}
+
+		FragmentManager fm = MainActivity.this.getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		Fragment fragment = null;
+
+		if (selectedItem.compareTo("Layout 1") == 0) {
+			fragment = new Layout1();
+		} else if (selectedItem.compareTo("Layout 2") == 0) {
+			fragment = new Layout2();
+		}
+
+		if (fragment != null) {
+			ft.replace(R.id.activity_main_content_fragment, fragment);
+			ft.commit();
+			tvTitle.setText(selectedItem);
+		}
+		mLayout.toggleMenu();
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (mLayout.isMenuShown()) {
+			mLayout.toggleMenu();
+		} else {
+			super.onBackPressed();
+		}
+	}
 }
